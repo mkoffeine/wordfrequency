0dd148317aa0e30aa6d1dd7153eb37122ce68c68
Michael <myhaylof@gmail.com>
2013-04-23 12:11:07 +0300

    crash fixes, using sdCard

--- wordfrequency/src/com/koffeine/wordfreq/WordFreqActivity.java
+++ wordfrequency/src/com/koffeine/wordfreq/WordFreqActivity.java
@@ -5,14 +5,19 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Environment;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -33,25 +38,31 @@ public class WordFreqActivity extends Activity {
         inText.addTextChangedListener(new OnValueChanged());
         outText = (EditText) findViewById(R.id.editTextResult);
         try {
+            long time = System.currentTimeMillis();
             initLogic();
+            long delta = System.currentTimeMillis() - time;
+            Toast toast = Toast.makeText(getApplicationContext(), "initLogic done. Delta: " + delta, Toast.LENGTH_LONG);
+            toast.show();
+            logger.debug("initLogic done. Delta: " + delta);
         } catch (IOException e) {
+            Toast toast = Toast.makeText(getApplicationContext(), "IOException during initLogic()", Toast.LENGTH_LONG);
+            toast.show();
             logger.debug(e.toString());
         }
-        Button btnClipboard =  (Button) findViewById(R.id.button_copy);
+        Button btnClipboard = (Button) findViewById(R.id.button_copy);
         btnClipboard.setOnClickListener(new ButtonBufferClick());
-        Button btnClear =  (Button) findViewById(R.id.button_clear);
+        Button btnClear = (Button) findViewById(R.id.button_clear);
         btnClear.setOnClickListener(new ButtonClearClick());
 
     }
 
     private void initLogic() throws IOException {
 
-        InputStream is = null;
-        String dictionaryFile = "en_full_raw.dic";
+        File sdCard = Environment.getExternalStorageDirectory();
+        String dictionaryFile = "en_full_raw_4.dic";
+        File file = new File(sdCard, "Android/data/com.koffeine.wordfreq/" + dictionaryFile);
 
-        is = getApplicationContext().getAssets().open(dictionaryFile);
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        BufferedReader br = new BufferedReader(new FileReader(file));
 
         String line;
         int i = 0;
@@ -69,11 +80,6 @@ public class WordFreqActivity extends Activity {
             }
         }
         wordsArray = words.toArray(new WordInfo[]{});
-        logger.debug("Start to sort");
-        long time = System.currentTimeMillis();
-//		Arrays.sort(wordsArray);
-        long delta = System.currentTimeMillis() - time;
-        logger.debug("Sorting is finished. Time: " + delta);
 
         for (WordInfo wordInfo : wordsArray) {
             if (i % 11000 == 0)
@@ -85,26 +91,26 @@ public class WordFreqActivity extends Activity {
         String s = inText.getText().toString();
         Log.d("123123", "123123 onTextChanged " + s);
         String status = "";
-        if (s.length() > 2) {
-//		int index = Arrays.binarySearch(wordsArray, new WordInfoEmulate(s));
+        if (s.length() > 2 && wordsArray != null && wordsArray.length > 0) {
             int index = Arrays.binarySearch(wordsArray, new WordInfo(s, 0, 0));
             index = index > -1 ? index : -index - 1;
-            WordInfo info = wordsArray[index];
-            status = "words:   " + info.toString() + "\n\n";
-            String[] suffixes = {"s", "ing", "es", "ly", "er", "ar", "ir", "y", "able", "e",
-                    "t", "ed", "en", "ist", "ful", "fy", "ian", "ize", "ible",
-                    "ness", "less", "ism", "hood", "ment"
-                    , "al", "ent", "nt", "sm", "m", "st", "an"
-                    , "ss", "ng", "n", "ble", "le", "r", "l"
-                    , "t", "ul", "sm", "n", "ood", "od", "d"};
-            for (String suffix : suffixes) {
-                String word = s + suffix;
-                index = Arrays.binarySearch(wordsArray, new WordInfo(word, 0, 0));
-                if (index > -1) {
-                    status += "   " + wordsArray[index].toString() + "\n";
-                }
-                else {
-                    //status += "   --no-- " + word + "\n";
+            if (index > -1 && index < wordsArray.length) {
+                WordInfo info = wordsArray[index];
+                status = "words:   " + info.toString() + "\n\n";
+                String[] suffixes = {"s", "ing", "es", "ly", "er", "ar", "ir", "y", "able", "e",
+                        "t", "ed", "en", "ist", "ful", "fy", "ian", "ize", "ible",
+                        "ness", "less", "ism", "hood", "ment"
+                        , "al", "ent", "nt", "sm", "m", "st", "an"
+                        , "ss", "ng", "n", "ble", "le", "r", "l"
+                        , "t", "ul", "sm", "n", "ood", "od", "d"};
+                for (String suffix : suffixes) {
+                    String word = s + suffix;
+                    index = Arrays.binarySearch(wordsArray, new WordInfo(word, 0, 0));
+                    if (index > 0 && index < wordsArray.length) {
+                        status += "   " + wordsArray[index].toString() + "\n";
+                    } else {
+                        //status += "   --no-- " + word + "\n";
+                    }
                 }
             }
         }
@@ -138,6 +144,7 @@ public class WordFreqActivity extends Activity {
             clipboard.setPrimaryClip(clip);
         }
     }
+
     private class ButtonClearClick implements View.OnClickListener {
 
         public void onClick(View view) {
---
+++ wordfrequency/src/com/koffeine/wordfreq/WordsModel.java
@@ -0,0 +1,15 @@
+package com.koffeine.wordfreq;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Michael
+ * Date: 23.04.13
+ * Time: 12:02
+ * To change this template use File | Settings | File Templates.
+ */
+public class WordsModel {
+    private Logger logger = Logger.getLogger(WordsModel.class.getSimpleName());
+    private WordInfo[] wordsArray;
+
+
+}

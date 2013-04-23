5f669fc36063b6cde836e20c4cc6c92bb16476cb
Michael <myhaylof@gmail.com>
2013-04-23 13:16:10 +0300

    moving logic to model

--- wordfrequency/src/com/koffeine/wordfreq/WordFreqActivity.java
+++ wordfrequency/src/com/koffeine/wordfreq/WordFreqActivity.java
@@ -5,7 +5,6 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.os.Bundle;
-import android.os.Environment;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
@@ -14,18 +13,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class WordFreqActivity extends Activity {
     private EditText inText;
     private EditText outText;
     private Logger logger = Logger.getLogger(WordFreqActivity.class.getSimpleName());
-    private WordInfo[] wordsArray;
+    WordsModel model;
 
     /**
      * Called when the activity is first created.
@@ -39,7 +33,7 @@ public class WordFreqActivity extends Activity {
         outText = (EditText) findViewById(R.id.editTextResult);
         try {
             long time = System.currentTimeMillis();
-            initLogic();
+            model = new WordsModel();
             long delta = System.currentTimeMillis() - time;
             Toast toast = Toast.makeText(getApplicationContext(), "initLogic done. Delta: " + delta, Toast.LENGTH_LONG);
             toast.show();
@@ -56,64 +50,11 @@ public class WordFreqActivity extends Activity {
 
     }
 
-    private void initLogic() throws IOException {
-
-        File sdCard = Environment.getExternalStorageDirectory();
-        String dictionaryFile = "en_full_raw_4.dic";
-        File file = new File(sdCard, "Android/data/com.koffeine.wordfreq/" + dictionaryFile);
-
-        BufferedReader br = new BufferedReader(new FileReader(file));
-
-        String line;
-        int i = 0;
-        ArrayList<WordInfo> words = new ArrayList<WordInfo>();
-        while ((line = br.readLine()) != null) {
-            i++;
-            int separatorIndex1 = line.indexOf(" ");
-            int separatorIndex2 = line.indexOf(" ", separatorIndex1 + 1);
-            String word = line.substring(0, separatorIndex1);
-            int freq = Integer.parseInt(line.substring(separatorIndex1 + 1, separatorIndex2));
-            int lineNumber = Integer.parseInt(line.substring(separatorIndex2 + 1));
-            words.add(new WordInfo(word, lineNumber, freq));
-            if (i % 11000 == 0) {
-                logger.debug("" + word + " " + i + "  # " + lineNumber + "  " + freq + "  ==== " + line);
-            }
-        }
-        wordsArray = words.toArray(new WordInfo[]{});
-
-        for (WordInfo wordInfo : wordsArray) {
-            if (i % 11000 == 0)
-                logger.debug(wordInfo.toString());
-        }
-    }
 
     private void updateStatus() {
         String s = inText.getText().toString();
         Log.d("123123", "123123 onTextChanged " + s);
-        String status = "";
-        if (s.length() > 1 && wordsArray != null && wordsArray.length > 0) {
-            int index = Arrays.binarySearch(wordsArray, new WordInfo(s, 0, 0));
-            index = index > -1 ? index : -index - 1;
-            if (index > -1 && index < wordsArray.length) {
-                WordInfo info = wordsArray[index];
-                status = "words:   " + info.toString() + "\n\n";
-                String[] suffixes = {"s", "ing", "es", "ly", "er", "ar", "ir", "y", "able", "e",
-                        "t", "ed", "en", "ist", "ful", "fy", "ian", "ize", "ible",
-                        "ness", "less", "ism", "hood", "ment"
-                        , "al", "ent", "nt", "sm", "m", "st", "an"
-                        , "ss", "ng", "n", "ble", "le", "r", "l"
-                        , "t", "ul", "sm", "n", "ood", "od", "d"};
-                for (String suffix : suffixes) {
-                    String word = s + suffix;
-                    index = Arrays.binarySearch(wordsArray, new WordInfo(word, 0, 0));
-                    if (index > 0 && index < wordsArray.length) {
-                        status += "   " + wordsArray[index].toString() + "\n";
-                    } else {
-                        //status += "   --no-- " + word + "\n";
-                    }
-                }
-            }
-        }
+        String status = model.getStatus(s);
         outText.setText(status);
     }
 
--- wordfrequency/src/com/koffeine/wordfreq/WordsModel.java
+++ wordfrequency/src/com/koffeine/wordfreq/WordsModel.java
@@ -1,15 +1,79 @@
 package com.koffeine.wordfreq;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Michael
- * Date: 23.04.13
- * Time: 12:02
- * To change this template use File | Settings | File Templates.
- */
+import android.os.Environment;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
 public class WordsModel {
     private Logger logger = Logger.getLogger(WordsModel.class.getSimpleName());
     private WordInfo[] wordsArray;
 
+    public WordsModel()  throws IOException{
+        initLogic();
+    }
+
+    public void initLogic() throws IOException {
+
+        File sdCard = Environment.getExternalStorageDirectory();
+        String dictionaryFile = "en_full_raw_4.dic";
+        File file = new File(sdCard, "Android/data/com.koffeine.wordfreq/" + dictionaryFile);
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String line;
+        int i = 0;
+        ArrayList<WordInfo> words = new ArrayList<WordInfo>();
+        while ((line = br.readLine()) != null) {
+            i++;
+            int separatorIndex1 = line.indexOf(" ");
+            int separatorIndex2 = line.indexOf(" ", separatorIndex1 + 1);
+            String word = line.substring(0, separatorIndex1);
+            int freq = Integer.parseInt(line.substring(separatorIndex1 + 1, separatorIndex2));
+            int lineNumber = Integer.parseInt(line.substring(separatorIndex2 + 1));
+            words.add(new WordInfo(word, lineNumber, freq));
+            if (i % 11000 == 0) {
+                logger.debug("" + word + " " + i + "  # " + lineNumber + "  " + freq + "  ==== " + line);
+            }
+        }
+        wordsArray = words.toArray(new WordInfo[]{});
+
+        for (WordInfo wordInfo : wordsArray) {
+            if (i % 11000 == 0)
+                logger.debug(wordInfo.toString());
+        }
+    }
+    public String getStatus(String typedWord) {
+        String status = "";
+        if (typedWord.length() > 1 && wordsArray != null && wordsArray.length > 0) {
+            int index = Arrays.binarySearch(wordsArray, new WordInfo(typedWord, 0, 0));
+            index = index > -1 ? index : -index - 1;
+            if (index > -1 && index < wordsArray.length) {
+                WordInfo info = wordsArray[index];
+                status = "words:   " + info.toString() + "\n\n";
+                String[] suffixes = {"s", "ing", "es", "ly", "er", "ar", "ir", "y", "able", "e",
+                        "t", "ed", "en", "ist", "ful", "fy", "ian", "ize", "ible",
+                        "ness", "less", "ism", "hood", "ment"
+                        , "al", "ent", "nt", "sm", "m", "st", "an"
+                        , "ss", "ng", "n", "ble", "le", "r", "l"
+                        , "t", "ul", "sm", "n", "ood", "od", "d"};
+                for (String suffix : suffixes) {
+                    String word = typedWord + suffix;
+                    index = Arrays.binarySearch(wordsArray, new WordInfo(word, 0, 0));
+                    if (index > 0 && index < wordsArray.length) {
+                        status += "   " + wordsArray[index].toString() + "\n";
+                    } else {
+                        //status += "   --no-- " + word + "\n";
+                    }
+                }
+            }
+        }
+        return status;
+    }
 
 }
